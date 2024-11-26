834d15a163fcef557d24cbb86706de1430facede
AlexanderAsaturyan <104361555+AlexanderAsaturyan@users.noreply.github.com>
2024-11-26 15:44:14 +0400

    Update SimpleTool.cs

--- SimpleTool.cs
+++ SimpleTool.cs
@@ -5,6 +5,6 @@ public class SimpleTool
     [MenuItem("Tools/My Custom Tool/Show Message")]
     private static void ShowMessage()
     {
-        EditorUtility.DisplayDialog("Hello", "This is a custom tool! UPGRADED!", "OK");
+        EditorUtility.DisplayDialog("Hello", "This is a custom tool! UPGRADED one more time!", "OK");
     }
 }
