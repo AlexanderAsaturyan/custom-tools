ff865e7995af9e43d072bda02a72e29a6ba787ca
AlexanderAsaturyan <104361555+AlexanderAsaturyan@users.noreply.github.com>
2024-11-26 12:35:38 +0400

    Initilal Commit.

---
+++ SimpleTool.cs
@@ -0,0 +1,10 @@
+using UnityEditor;
+
+public class SimpleTool 
+{
+    [MenuItem("Tools/My Custom Tool/Show Message")]
+    private static void ShowMessage()
+    {
+        EditorUtility.DisplayDialog("Hello", "This is a custom tool!", "OK");
+    }
+}
---
+++ Third Party Notices.md
@@ -0,0 +1,4 @@
+
+# Third-Party Notices
+
+ This package does not use any third-party libraries.
